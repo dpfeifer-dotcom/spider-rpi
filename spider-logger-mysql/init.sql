7994b80912e7f19e6d46aa12e2177cee12f89416
pfeifer.daniel83@gmail.com <pfeifer.daniel83@gmail.com>
2025-10-04 21:21:07 +0200

    sql

--- spider-logger-mysql/init.sql
+++
@@ -1,8 +0,0 @@
-USE spider-logger;
-
-CREATE TABLE IF NOT EXISTS error_logs (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    component VARCHAR(128),
-    error TEXT
-);
